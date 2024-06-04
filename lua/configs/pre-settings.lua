cf749b84c57d3fc0dbe18fe2b041ba68ea56b8a7
Sunghee Kim <h33uid@gmail.com>
2024-06-05 01:33:42 +0900

    add pre-options
    1. set scroll offsets for lines & columns
    2. no swap file

--- lua/configs/pre-settings.lua
+++ lua/configs/pre-settings.lua
@@ -2,6 +2,10 @@ vim.g.colorscheme = "catppuccin"
 vim.g.mapleader = " "
 vim.g.localledaer = "\\"
 
+vim.opt.scrolloff = 4 -- lines
+vim.opt.sidescrolloff = 8 -- columns
+vim.opt.autoread = true
+vim.opt.undofile = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.autoindent = true
